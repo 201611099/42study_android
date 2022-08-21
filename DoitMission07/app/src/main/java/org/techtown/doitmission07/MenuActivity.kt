f7162662ed21b257306cfddec5124c44e3110dc0
hyojlee <hyojlee@student.42seoul.kr>
2022-08-21 22:14:18 +0900

    show Toast

--- DoitMission07/app/src/main/java/org/techtown/doitmission07/MenuActivity.kt
+++ DoitMission07/app/src/main/java/org/techtown/doitmission07/MenuActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,16 +17,19 @@ class MenuActivity : AppCompatActivity() {
 
         btnCustomer.setOnClickListener(View.OnClickListener {
             val intent: Intent = Intent(this, CustomerActivity::class.java)
+            Toast.makeText(this, "고객 관리", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         })
 
         btnSales.setOnClickListener(View.OnClickListener {
             val intent: Intent = Intent(this, SalesActivity::class.java)
+            Toast.makeText(this, "매출 관리", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         })
 
         btnProduct.setOnClickListener(View.OnClickListener {
             val intent: Intent = Intent(this, ProductActivity::class.java)
+            Toast.makeText(this, "상품 관리", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         })
     }
